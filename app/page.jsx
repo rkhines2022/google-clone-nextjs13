4c8ae6ee48bbe3db80316c61a7161f79feb8a7cb
Ray Hines <rkhines@cox.net>
2023-11-28 12:22:08 -0700

    Add Home Header Component

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
+import HomeHeader from './components/HomeHeader'
+
 
 export default function Home() {
   return (
-    <div className='text-red-600 text-3xl'>Home</div>
+    <HomeHeader />
   )
 }
