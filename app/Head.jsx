4b083b0bd4a0714985471a90c5200cd54787d896
Ray Hines <rkhines@cox.net>
2023-11-28 10:55:19 -0700

    Install Tailwind css

--- app/Head.jsx
+++ app/Head.jsx
@@ -6,7 +6,7 @@ export default function Head() {
     <title>Google Clone Next js 13</title>
     <meta content="width=device-width, initial-scale=1" name="viewport"/>
     <meta name="description" content="Google Clone Created by next js 13"/>
-    <link  rel="icon" href="/favicon.ico" />
+    <link  rel="icon" href="/icon.svg" />
     </>
   )
 }
