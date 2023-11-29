93763cab9c4b901379e0e198333c7bff271ae81e
Ray Hines <rkhines@cox.net>
2023-11-28 18:35:16 -0700

    Add Body Section

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
-import HomeHeader from './components/HomeHeader'
-
+import React from "react";
+import HomeHeader from "./components/HomeHeader";
+import Image from "next/image";
+import HomeSearch from "./components/HomeSearch";
 
 export default function Home() {
   return (
-    <HomeHeader />
-  )
+    <>
+      {/* Header */}
+      <HomeHeader />
+
+      {/* Body */}
+      <div className="flex flex-col items-center mt-24">
+        <Image
+        width="300"
+        height="100" 
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/640px-Google_2015_logo.svg.png"
+        />
+        <HomeSearch />
+      </div>
+    </>
+  );
 }
